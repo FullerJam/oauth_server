59269de3f75726914efb9464ab00ee52dee7b043
FullerJam <2fullj37@solent.ac.uk>
2020-02-22 13:55:56 +0000

    added repository classes yet to adjust, written database access class, currently testing

---
+++ classes/repositories/UserRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+use League\OAuth2\Server\Entities\ClientEntityInterface;
+use League\OAuth2\Server\Repositories\UserRepositoryInterface;
+use OAuth2ServerExamples\Entities\UserEntity;
+
+class UserRepository implements UserRepositoryInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getUserEntityByUserCredentials(
+        $username,
+        $password,
+        $grantType,
+        ClientEntityInterface $clientEntity
+    ) {
+        if ($username === 'alex' && $password === 'whisky') {
+            return new UserEntity();
+        }
+
+        return;
+    }
+}
