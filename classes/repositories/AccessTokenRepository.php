1b131b230beca20fe54e2fb53a5b36ce27840064
unknown <2fullj37@solent.ac.uk>
2020-04-04 14:53:00 +0100

    created access token repo

--- classes/repositories/AccessTokenRepository.php
+++ classes/repositories/AccessTokenRepository.php
@@ -3,7 +3,6 @@
 use League\OAuth2\Server\Entities\AccessTokenEntityInterface;
 use League\OAuth2\Server\Entities\ClientEntityInterface;
 use League\OAuth2\Server\Repositories\AccessTokenRepositoryInterface;
-use OAuth2ServerExamples\Entities\AccessTokenEntity;
 
 class AccessTokenRepository implements AccessTokenRepositoryInterface
 {
@@ -19,6 +18,10 @@ class AccessTokenRepository implements AccessTokenRepositoryInterface
     public function persistNewAccessToken(AccessTokenEntityInterface $accessTokenEntity)
     {
         // Some logic here to save the access token to a database
+        $suppliedScopes = implode(" ", $accessTokenEntity->getScopes()); //implode the scopes array so it can be saved to a database
+        $sql = "INSERT INTO access_tokens(access_token, token_expires, user_id, scope, client_id) VALUES (?,?,?,?,?)";
+        $stmt = $this->conn->prepare($sql); 
+        $stmt->execute([$accessToken->getIdentifier(), $accessToken->getExpiryDateTime()->getTimestamp(), $accessToken->getUserIdentifier(), $scopes, $accessToken->getClient()->getIdentifier()]);
     }
 
     /**
@@ -26,7 +29,9 @@ class AccessTokenRepository implements AccessTokenRepositoryInterface
      */
     public function revokeAccessToken($tokenId)
     {
-        // Some logic here to revoke the access token
+        $sql = "UPDATE access_tokens SET is_revoked=true WHERE access_token=?";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute([$tokenId]);
     }
 
     /**
@@ -34,7 +39,10 @@ class AccessTokenRepository implements AccessTokenRepositoryInterface
      */
     public function isAccessTokenRevoked($tokenId)
     {
-        return false; // Access token hasn't been revoked
+        $sql = "SELECT revoked FROM access_tokens WHERE access_token=$tokenId";
+        $stmt = $this->conn->prepare($sql);
+        $row = $stmt->fetch();
+        return $row["is_revoked"];
     }
 
     /**
