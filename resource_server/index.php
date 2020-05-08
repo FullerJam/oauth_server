8c996195f59b8977aad62d1f7c511731b102766d
unknown <2fullj37@solent.ac.uk>
2020-05-08 13:51:10 +0100

    simple routes for message board

--- resource_server/index.php
+++ resource_server/index.php
@@ -1,8 +1,7 @@
 <?php
-error_reporting(E_ALL & ~E_NOTICE); // ignore all notices, as was corrupting json responses with "public key permissions are not correct, recommend changing to 600 or 660 instead of 666. Would be a problem with real auth server but as this is a proof of concept just ignoring it. 
-use Psr\Http\Message\ResponseInterface as Response; //slim req & res interfaces
+error_reporting(E_ALL & ~E_NOTICE); // ignore all notices, as was corrupting json responses with "public key permissions are not correct, recommend changing to 600 or 660 instead of 666. Would be a problem with real auth server but as this is a proof of concept just ignoring it.
+use League\OAuth2\Server\CryptKey; //slim req & res interfaces
 use Psr\Http\Message\ServerRequestInterface as Request;
-use League\OAuth2\Server\CryptKey;
 // Include all the Slim dependencies. Composer creates an 'autoload.php' inside
 // the 'vendor' directory which will, in turn, include all required dependencies.
 require '../vendor/autoload.php';
@@ -23,10 +22,8 @@ $container['db'] = function () {
     return $conn;
 };
 
-
-
 // Setup the resource server
-$container['server'] = function ($container) use($app) {
+$container['server'] = function ($container) use ($app) {
     $db = $container->get('db');
     $publicKeyPath = new CryptKey('C:\xampp\htdocs\oauth\oauth_server\authorisation_server\public.key', null, false);
     $server = new \League\OAuth2\Server\ResourceServer(
@@ -46,26 +43,74 @@ $app->add(new \League\OAuth2\Server\Middleware\ResourceServerMiddleware($server)
 // oauth_scopes - an array of string scope identifiers
 // If the authorization is invalid an instance of OAuthServerException::accessDenied will be thrown.
 
-
 // Setup up routes
 $app->post('/read', function ($request, $response, array $args) {
-    try{
-        if(in_array("email", $request->getAttribute("oauth_scopes")))
-        $user_id = $request->getAttribute("oauth_user_id");
-        $sql = "SELECT email FROM users WHERE id=?";
-        $ps = $this->db->prepare($sql);
-        $ps->execute([$user_id]);
-        $result = $ps->fetch();
-        return $response->withJson(['email' => $result["email"]], 200);
-    }catch(\Exception $exception){
-        return $response->withJson(["msg"=>$e->getMessage()], 500);
+    try {
+        if (in_array("email", $request->getAttribute("oauth_scopes"))) {
+            $user_id = $request->getAttribute("oauth_user_id");
+            $sql = "SELECT email FROM users WHERE id=?";
+            $ps = $this->db->prepare($sql);
+            $ps->execute([$user_id]);
+            $result = $ps->fetch();
+            return $response->withJson(['email' => $result["email"], 'username' => "James"], 200);
+        }
+    } catch (\Exception $exception) {
+        return $response->withJson(["msg" => $e->getMessage()], 500);
     }
 
 });
 
-$app->post('/readwrite', function ($request, $response, array $args)  {
-    return $response->withJson(['msg' => 'Read & Write permissions route'], 200);
+$app->post('/get_messages', function ($request, $response, array $args) {
+
+    try {
+        if (in_array("email", $request->getAttribute("oauth_scopes"))) {
+            $sql = "SELECT * FROM message_board";
+            $ps = $this->db->prepare($sql);
+            $ps->execute();
+            $rows = $ps->fetchAll(PDO::FETCH_ASSOC);
+            if (count($rows) < 1) {
+                return $response->withJson(["msg" => "No messages to display"], 204);
+            } else {
+                return $response->withJson($rows);
+            }
+            // return $response->withJson(['id' => $result["id"], 'email' => $result['email'], 'time' => $result['time'], 'message' => $result['message']], 200);
+        }
+    } catch (\Exception $exception) {
+        return $response->withJson(["msg" => $e->getMessage()], 500);
+    }
 });
 
+$app->post('/set_message', function ($request, $response, array $args) {
+
+    try {
+        if (in_array("email", $request->getAttribute("oauth_scopes"))) {
+            $message = $_POST['msgArray']; //message
+            $date = new DateTime(); // time
+            $user_id = $request->getAttribute("oauth_user_id"); //userId
+            $sql = "SELECT email FROM users WHERE id=?";
+            $ps = $this->db->prepare($sql);
+            $ps->execute([$user_id]);
+            $result = $ps->fetch();
+
+            $sql2 = "INSERT INTO message_board (userId, email, time, message) VALUES ('?','?','?','?')";
+            $ps2 = $this->db->prepare($sql2);
+            $ps2->execute([$user_id, $result['email'], $date->getTimestamp(), $message[0]]);
+        }
+    } catch (\Exception $exception) {
+        return $response->withJson(["msg" => $e->getMessage()], 500);
+    }
+});
+
+$app->post('/del_message', function ($request, $response, array $args) {
+
+    try {
+        if (in_array("email", $request->getAttribute("oauth_scopes"))) {
+           
+
+        }
+    } catch (\Exception $exception) {
+        return $response->withJson(["msg" => $e->getMessage()], 500);
+    }
+});
 // Run the application
 $app->run();
